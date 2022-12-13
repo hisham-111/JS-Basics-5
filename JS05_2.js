0e3ec197166487d554d7059e69d7c83c2eaabce3
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-13 16:07:57 +0200

    condition

---
+++ JS05_2.js
@@ -0,0 +1,11 @@
+var Name = prompt("what is your name ? ")
+if (Name == "Alice"){
+alert(`hello ${Name}`)
+}
+else if (Name == "Bob"){
+    alert(`hello ${Name}`)
+    }
+
+else {
+    alert ("Hello Stranger !")
+}
